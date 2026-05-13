44996992391cdd3f70b6f455f255a4bb72bded79
suryansh sah <82665945+suryanshsah1665@users.noreply.github.com>
2026-05-14 02:50:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    db_url = os.environ.get("DATABASE_URL")
+
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SECRET_KEY = os.environ.get("SECRET_KEY")
